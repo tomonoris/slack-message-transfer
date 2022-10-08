b0f9e481f2501a403d16667592cb1ffb9a835e7e
tomonorisuzuk1 <tomonorisuzuki@outlook.jp>
2022-10-08 20:42:58 +0900

    s3リソースの追加

--- main.tf
+++ main.tf
@@ -12,3 +12,12 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_s3_bucket" "lambda_source_bucket" {
+  bucket = "slack-message-transfer-lambda-source-bucket"
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.lambda_source_bucket.id
+  acl = "private"
+}
