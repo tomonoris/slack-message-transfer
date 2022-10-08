e67595fd5256df186ab20ee7c0f06ac73d216b0b
tomonorisuzuk1 <tomonorisuzuki@outlook.jp>
2022-10-08 20:56:16 +0900

    s3にソースファイルをアップロードするように変更

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 4.0.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
   }
 
   required_version = "~> 1.0"
@@ -13,6 +17,12 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "archive_file" "lambda_source" {
+  source_dir = "${path.module}/src"
+  output_path = "${path.module}/source.zip"
+  type        = "zip"
+}
+
 resource "aws_s3_bucket" "lambda_source_bucket" {
   bucket = "slack-message-transfer-lambda-source-bucket"
 }
@@ -20,4 +30,11 @@ resource "aws_s3_bucket" "lambda_source_bucket" {
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.lambda_source_bucket.id
   acl = "private"
+}
+
+resource "aws_s3_object" "lambda_source" {
+  bucket = aws_s3_bucket.lambda_source_bucket.id
+  key    = "source.zip"
+  source = data.archive_file.lambda_source.output_path
+  etag = filemd5(data.archive_file.lambda_source.output_path)
 }
